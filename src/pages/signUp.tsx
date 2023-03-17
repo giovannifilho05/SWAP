b383980de0a7d0b4b7b36be187bf36908de5c863
Giovanni <giovannifilho0905@hotmail.com>
2023-03-16 23:39:58 -0300

    Fix credentials bug

--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -1,10 +1,16 @@
 import { NextPage } from "next";
+import Head from "next/head";
 
 const SignUp: NextPage = () => {
     return (
+        <>
+        <Head>
+            <title>Crie sua conta</title>
+        </Head>
         <h1>
             SignUp
         </h1>
+        </>
     )
 }
 
--- src/services/firebase-admin.ts
+++ src/services/firebase-admin.ts
@@ -1,12 +1,11 @@
 import * as firebaseAdmin from 'firebase-admin';
-import credentials from '../../firebase-admin-credentials.json'
 
 if (!firebaseAdmin.apps.length) {
     firebaseAdmin.initializeApp({
         credential: firebaseAdmin.credential.cert({
-            privateKey: credentials.private_key,
-            clientEmail: credentials.client_email,
-            projectId: credentials.project_id,
+            privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+            clientEmail: process.env.FIREBASE_ADMIN_CLIENTE_EMAIL,
+            projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
         })
     });
 }
